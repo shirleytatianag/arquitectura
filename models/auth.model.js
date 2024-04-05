29765b09c539ab0dfeb2293ff4e1667507476002
Shirley Tatiana García <shirleygarcia@wposs.con>
2024-04-05 08:07:41 -0500

    ajustes

--- models/auth.model.js
+++ models/auth.model.js
@@ -1,7 +1,11 @@
 import pgService from "../services/pg.service.js";
 
 export const getUser = async (authLoginReq)=>{
-    const pg = new pgService();
-    return await pg.connection.oneOrNone(`SELECT USER_ID FROM USERS
-    WHERE USER_NAME = $1 AND USER_PASSWORD = $2`, [authLoginReq.username, authLoginReq.password])
+    try {
+        const pg = new pgService();
+        return await pg.connection.oneOrNone(`SELECT USER_ID FROM USERS
+        WHERE USER_NAME = $1 AND USER_PASSWORD = $2`, [authLoginReq.username, authLoginReq.password])
+    } catch (error) {
+        return 'Ha habido un error, intenta más tarde' + error;
+    }
 }
