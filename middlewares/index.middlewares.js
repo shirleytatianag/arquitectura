7c94b1b6139ca8532a196ab45b3563e7ea3015b4
Shirley Tatiana García Carrascal <shirleygarcia@wposs.con>
2024-06-19 11:18:47 -0500

    validaciones

--- middlewares/index.middlewares.js
+++ middlewares/index.middlewares.js
@@ -3,7 +3,8 @@ import {verifyToken} from "./token.middleware.js";
 
 const middleware = Router();
 
-//Proteger todo lo que entra primero entra al middleware
+// Proteger  lo que entra primero entra al middleware
 middleware.use("/producto", verifyToken)
+middleware.use("/category", verifyToken)
 
 export default middleware
